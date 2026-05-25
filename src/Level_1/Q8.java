fb100b03a2e30a8ebe4b1996c68b4d557d3e81ca
Paras Surti <paras16surti@gmail.com>
2026-05-26 00:21:51 +0530

    Q8.Take a temperature value and print Cold, Warm, or Hot using range conditions.

---
+++ src/Level_1/Q8.java
@@ -0,0 +1,27 @@
+//Take a temperature value and print “Cold”, “Warm”, or “Hot” using range conditions.
+package Level_1;
+
+import java.util.Scanner;
+
+public class Q8
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Temperature ");
+        float temp = input.nextFloat();
+
+        if(temp > 30 && temp < 40)
+        {
+            System.out.println(temp + "°C is hot");
+        }
+        else if(temp > 20 && temp < 30)
+        {
+            System.out.println(temp + "°C is warm");
+        }
+        else if (temp > 0 && temp <10)
+        {
+            System.out.println(temp + "°C is cold");
+        }
+    }
+}
