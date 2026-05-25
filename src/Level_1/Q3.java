cf8c56bd115582e4abc6b102aa85b3901c44280c
Paras Surti <paras16surti@gmail.com>
2026-05-25 23:27:23 +0530

    Q3.Check if a number is divisible by 5.

---
+++ src/Level_1/Q3.java
@@ -0,0 +1,23 @@
+//Check if a number is divisible by 5
+package Level_1;
+
+import java.util.Scanner;
+
+public class Q3
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Number : ");
+        int num = input.nextInt();
+
+        if(num % 5 == 0)
+        {
+            System.out.println(num + " is divisible by 5.");
+        }
+        else
+        {
+            System.out.println(num + " is not divisible by 5.");
+        }
+    }
+}
