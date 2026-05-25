74649993da311535da1242b56774d9c53f427711
Paras Surti <paras16surti@gmail.com>
2026-05-25 23:54:05 +0530

    Q6.Take two numbers and print the larger one.

---
+++ src/Level_1/Q6.java
@@ -0,0 +1,24 @@
+//Take two numbers and print the larger one.
+package Level_1;
+
+import java.util.Scanner;
+
+public class Q6
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Two Numbers : ");
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+
+        if(num1 > num2)
+        {
+            System.out.println(num1 + " is the largest number.");
+        }
+        else
+        {
+            System.out.println(num2 + " is the largest number.");
+        }
+    }
+}
