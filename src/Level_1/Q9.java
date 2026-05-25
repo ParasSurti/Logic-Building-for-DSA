3b08a84c76627a0add8938fbc66ef3afb8c97772
Paras Surti <paras16surti@gmail.com>
2026-05-26 00:33:44 +0530

    Q9.Take a character and check if it's a vowel or consonant.

---
+++ src/Level_1/Q9.java
@@ -0,0 +1,23 @@
+//Take a character and check if it’s a vowel or consonant.
+package Level_1;
+
+import java.util.Scanner;
+
+public class Q9
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter an Character : ");
+        String ch = input.next();
+
+        if(ch.equals("a") || ch.equals("e") || ch.equals("i") || ch.equals("o") || ch.equals("u"))
+        {
+            System.out.println(ch + " is a vowel.");
+        }
+        else
+        {
+            System.out.println(ch + " is a consonant.");
+        }
+    }
+}
