826446acd243eb8f6cb422408dea31d77d3ec3be
Paras Surti <paras16surti@gmail.com>
2026-05-25 22:59:44 +0530

    Q1 completed

---
+++ src/Level_1/Q1.java
@@ -0,0 +1,27 @@
+//Take a number and print whether it’s positive, negative, or zero.
+
+package Level_1;
+
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main ()
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Number: ");
+        int num = input.nextInt();
+
+        if(num > 0)
+        {
+            System.out.println( num + " is a positive number");
+        }
+        else if(num < 0 )
+        {
+            System.out.println(num + " is a negative number");
+        }
+        else
+        {
+            System.out.println(num + " is an zero");
+        }
+    }
+}
