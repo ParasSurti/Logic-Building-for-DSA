ac7927628a909a6b3a59ddaf071ac7a41face115
Paras Surti <paras16surti@gmail.com>
2026-05-26 23:00:37 +0530

    Q4.Check if one of two given numbers is a multiple of the other.

---
+++ src/Level_2/Q4.java
@@ -0,0 +1,28 @@
+//Check if one of two given numbers is a multiple of the other.
+package Level_2;
+
+import java.util.Scanner;
+
+public class Q4
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the two numbers : ");
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+
+        if( num1 % num2 == 0.0)
+        {
+            System.out.println(num1 +" is the multiply of " + num2);
+        }
+        else if(num2 % num1 == 0.0)
+        {
+            System.out.println( num2 + " is the multiply of " + num1);
+        }
+        else
+        {
+            System.out.println(num1 + ", " + num2 + " both aren't each other's multiply.");
+        }
+    }
+}
