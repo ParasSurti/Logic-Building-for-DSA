62877eaec1f79c00c98343fd2528024785d71b2c
Paras Surti <paras16surti@gmail.com>
2026-05-26 23:14:34 +0530

    Q5.Take the hour of the day (0-23) and print Good Morning, Good Afternoon, Good Evening or Good Night.

---
+++ src/Level_2/Q5.java
@@ -0,0 +1,31 @@
+//Take the hour of the day (0–23) and print “Good Morning”, “Good Afternoon”, “GoodEvening”, or “Good Night”.
+package Level_2;
+
+import java.util.Scanner;
+
+public class Q5
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the time of the day : ");
+        float hr = input.nextFloat();
+
+        if(Math.round(hr) >= 0 && Math.round(hr) <= 11.59)
+        {
+            System.out.println("Current time is " + hr + "\n !GOOD MORNING! " );
+        }
+        else if(Math.round(hr) >=12 && Math.round(hr) <= 17.59)
+        {
+            System.out.println("Current time is " + hr + "\n !GOOD AFTERNOON! ");
+        }
+        else if(Math.round(hr) >=18 && Math.round(hr) <= 21.59)
+        {
+            System.out.println("Current time is " + hr + "\n !GOOD AFTERNOON! ");
+        }
+        else
+        {
+            System.out.println("Current time is " + hr + "\n !GOOD NIGHT! ");
+        }
+    }
+}
