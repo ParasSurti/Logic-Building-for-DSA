2f4d70cb86ba1f432bf3b0801b297707ef4e7329
Paras Surti <paras16surti@gmail.com>
2026-05-26 12:53:00 +0530

    Q1.Take three sides and check if they form a valid triangle.

---
+++ src/Level_2/Q1.java
@@ -0,0 +1,25 @@
+package Level_2;
+
+import java.util.Scanner;
+
+public class Q1
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the side's : ");
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+
+
+        if(a + b > c && a + c > b && b + c > a)
+        {
+            System.out.println("With the given three sides [" + a + "," + b +","+ c +"], We can form a valid triangle. ");
+        }
+        else
+        {
+            System.out.println("With the given three sides[" + a + "," + b +","+ c +"], we cannot form a valid triangle. ");
+        }
+    }
+}
