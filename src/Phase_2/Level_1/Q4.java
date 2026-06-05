c2e6a7d95c7c598595c599c4df3443eeadda69b7
Paras Surti <paras16surti@gmail.com>
2026-06-05 15:10:52 +0530

    Q4.Print numbers from 10 down to 1.

---
+++ src/Phase_2/Level_1/Q4.java
@@ -0,0 +1,15 @@
+//Print numbers from 10 down to 1.
+package Phase_2.Level_1;
+
+public class Q4
+{
+    public static void main(String[] args)
+    {
+        int start = 10;
+        int end = 1;
+        for (start = 10; start >= end; start--)
+        {
+            System.out.println(start);
+        }
+    }
+}
