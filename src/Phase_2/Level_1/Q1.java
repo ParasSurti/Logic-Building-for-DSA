c05ebbfc05e8b80f428626ed923608b54d6dee35
Paras Surti <paras16surti@gmail.com>
2026-06-05 15:06:10 +0530

    Q1.Print numbers from 1 to 10.

---
+++ src/Phase_2/Level_1/Q1.java
@@ -0,0 +1,14 @@
+//Print numbers from 1 to 10.
+package Phase_2.Level_1;
+
+public class Q1
+{
+    public static void main(String[] args)
+    {
+        int num = 10;
+        for (int i = 1; i <= num; i++)
+        {
+            System.out.println("Number : " + i);
+        }
+    }
+}
