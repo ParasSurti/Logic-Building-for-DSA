bd7cf88c46fc408d9e40ba023acce26078da36b2
Paras Surti <paras16surti@gmail.com>
2026-06-07 22:15:53 +0530

    Q7.Find the sum of all factors of a number.

--- src/Phase_2/Level_3/Q7.java
+++ src/Phase_2/Level_3/Q7.java
@@ -1,4 +1,4 @@
-//Check if a number is a strong number (sum of factorials of digits == number).
+//Find the sum of all factors of a number.
 package Phase_2.Level_3;
 
 import java.util.Scanner;
@@ -8,36 +8,18 @@ public class Q7
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter a number : ");
+        System.out.print("Enter the number : ");
         int num = input.nextInt();
-        int value = num;
 
         int sum = 0;
-        while(num != 0)
+        for (int i = 1; i <= num ; i++)
         {
-            int temp = num % 10;
-
-            //METHOD CALL
-            sum += Factorial(temp);
-            num = num / 10;
+            boolean isFactor = num % i == 0;
+            if(isFactor)
+            {
+                sum += i;
+            }
         }
-        boolean strong = sum == value;
-        if(strong)
-        {
-            System.out.println(value + " is a strong number!!!");
-        }
-        else
-        {
-            System.out.println(value + " isn't a strong number!!!");
-        }
-    }
-    public static int Factorial(int temp)
-    {
-        int product = 1;
-        for (int j = temp; j > 1; j--)
-        {
-            product = product * j;
-        }
-        return product;
+        System.out.print("Sum of all factors of a " + num + " : " + sum);
     }
 }
