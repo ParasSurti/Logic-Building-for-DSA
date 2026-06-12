e231b48a5489832924f53ae68c5560ef038ea076
Paras Surti <paras16surti@gmail.com>
2026-06-12 13:59:22 +0530

    Q4.Find product of digits of a number recursively.

--- src/Phase_3/Level_2/Q04.java
+++ src/Phase_3/Level_2/Q04.java
@@ -11,23 +11,22 @@ public class Q04
         System.out.print("Enter the Number : ");
         int num = input.nextInt();
         int ref_num = num;
-        int store = 1;
         int pro = 1;
-        digitProduct(num,store,ref_num,pro);
+        digitProduct(num,ref_num,pro);
     }
-    static void digitProduct(int num,int store,int ref_num, int pro)
+    static void digitProduct(int num,int ref_num, int pro)
     {
         if(num !=0)
         {
-            store = num % 10;
+            pro *= num % 10;
             num /= 10;
-            pro *=store;
+
         }
         if(num == 0)
         {
             System.out.println("Product of the  [" + ref_num + "] Digits is " + pro);
             return;
         }
-        digitProduct(num, store, ref_num, pro);
+        digitProduct(num, ref_num, pro);
     }
 }
