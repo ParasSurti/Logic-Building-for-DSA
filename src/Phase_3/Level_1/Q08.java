fc42782560978c05612e594ef928f94e4df8721b
Paras Surti <paras16surti@gmail.com>
2026-06-12 11:27:59 +0530

    Q8.Find nth Fibonacci number recursively.

---
+++ src/Phase_3/Level_1/Q08.java
@@ -0,0 +1,43 @@
+//Find nth Fibonacci number recursively
+package Phase_3.Level_1;
+
+import java.util.Scanner;
+
+public class Q08
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the nth number : ");
+        int n = input.nextInt();
+
+        int a = 0;
+        int b = 1;
+        int sum = 0;
+        //logic
+//        for (int i = 0; i <= n ; i++)
+//        {
+//            System.out.println(sum);
+//            sum = a + b;
+//            a = b;
+//            b = sum;
+//        }
+
+        printFibo(n,a,b,sum,0);
+    }
+    static void printFibo(int n , int a,int b,int sum, int count)
+    {
+        System.out.println(sum);
+        sum = a + b;
+        a = b;
+        b = sum;
+
+        //exit Recursion (Base case)
+        if(count == n)
+        {
+            return;
+        }
+        count++;
+        printFibo(n,a,b,sum,count);
+    }
+}
