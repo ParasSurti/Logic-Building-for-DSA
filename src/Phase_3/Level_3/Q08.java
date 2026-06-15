57f1ae275d4ad5041723a0f11cc07dbe8969820a
Paras Surti <paras16surti@gmail.com>
2026-06-15 13:11:52 +0530

    Q8.Print numbers in increasing and decreasing order in same function.

---
+++ src/Phase_3/Level_3/Q08.java
@@ -0,0 +1,27 @@
+//Print numbers in increasing and decreasing order in same function.
+package Phase_3.Level_3;
+
+import java.util.Scanner;
+
+public class Q08
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int n = input.nextInt();
+
+        printBoth(1,n);
+    }
+    static void printBoth(int count,int n)
+    {
+        if(count == n)
+        {
+            System.out.print(count + " ");
+            return;
+        }
+        System.out.print(count + " ");
+        printBoth(count + 1,n);
+        System.out.print(count + " ");
+    }
+}
