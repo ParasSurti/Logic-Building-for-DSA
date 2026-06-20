2e9eccb37ea85a873b88ce6a2d52f175ec3cef37
Paras Surti <paras16surti@gmail.com>
2026-06-20 20:18:13 +0530

    Q6.Find the sum of even elements only.

---
+++ src/Phase_4/Level_2/Q06.java
@@ -0,0 +1,34 @@
+//Find the sum of even elements only.
+package Phase_4.Level_2;
+
+import java.util.Scanner;
+
+public class Q06
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Size of Arrays : ");
+        int n = input.nextInt();
+
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = input.nextInt();
+        }
+
+        int Evensum = sum(arr,0,n - 1);
+        System.out.print("The sum of the even number present in arrays : " + Evensum);
+    }
+    static int sum(int[] arr, int sum, int n)
+    {
+        if(n < 0) return 0;
+        sum = sum(arr,sum,n - 1);
+        if(arr[n] % 2 == 0)
+        {
+            sum += arr[n];
+        }
+        return sum;
+    }
+}
