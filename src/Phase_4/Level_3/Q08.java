3ff67675b668004f069216ed2feefe1400369442
Paras Surti <paras16surti@gmail.com>
2026-06-21 22:48:45 +0530

    Q8.Rotate an array by one position to the right.

---
+++ src/Phase_4/Level_3/Q08.java
@@ -0,0 +1,36 @@
+//Rotate an array by one position to the right.
+package Phase_4.Level_3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q08
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the size of the Array : ");
+        int n = input.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = input.nextInt();
+        }
+
+        int[] newArr = new int[n];
+        right(arr,newArr,n - 1,1);
+        System.out.println(Arrays.toString(newArr));
+    }
+    static int right(int[] arr, int[] newArr , int n, int index)
+    {
+        if(n < 0) return index;
+        index = right(arr,newArr,n - 1, index);
+        if(n == arr.length - 1)
+        {
+            return newArr[0] = arr[n];
+        }
+        newArr[index] = arr[n];
+        return index += 1;
+    }
+}
