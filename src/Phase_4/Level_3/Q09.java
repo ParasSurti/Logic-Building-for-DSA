fa97807255b25a53e1f5647410c4a3799f919c67
Paras Surti <paras16surti@gmail.com>
2026-06-22 11:30:39 +0530

    Q9.Swap alternate elements (1st ↔ 2nd, 3rd ↔ 4th, etc).

--- src/Phase_4/Level_3/Q09.java
+++ src/Phase_4/Level_3/Q09.java
@@ -9,7 +9,7 @@ public class Q09
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter the number : ");
+        System.out.print("Enter the size of the Array : ");
         int n = input.nextInt();
 
         int[] arr = new int[n];
