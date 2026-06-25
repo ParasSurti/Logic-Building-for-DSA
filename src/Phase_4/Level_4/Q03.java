fbcb32e96ac2cd0b3f8d818c05f875ae6534d1d7
Paras Surti <paras16surti@gmail.com>
2026-06-25 21:00:47 +0530

    Q3.Merge two arrays into a third array.

--- src/Phase_4/Level_4/Q03.java
+++ src/Phase_4/Level_4/Q03.java
@@ -4,48 +4,30 @@ package Phase_4.Level_4;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Q03
+public class Q03 extends ArrayTemplate
 {
+
     public static void main(String[] args)
     {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the size of the Arrays : ");
-        int n = input.nextInt();
-
-        //First Array
-        int[] arr1 = new int[n];
-        for (int i = 0; i < n; i++)
-        {
-            arr1[i] = input.nextInt();
-        }
-
-        //Second Array
-        int[] arr2 = new int[n];
-        for (int i = 0; i < n; i++)
-        {
-            arr2[i] = input.nextInt();
-        }
-
-        int merge_n = n * 2;
-        int[] merge_arr = new int[merge_n];
-        merge(arr1,arr2,merge_arr, n - 1);
-        System.out.println(Arrays.toString(merge_arr));
+        Q03 q = new Q03();
+        q.setup();
+        q.solve();
     }
-    static void merge(int[] arr1, int[] arr2,int[] merge, int n)
+
+    @Override
+    void solve()
     {
-        if(n < 0 ) return ;
-        fillArr1(arr1,merge,n);
-        fillArr2(arr2,merge,n);
-        merge(arr1, arr2, merge, n - 1);
+        int[] arr3 = new int[n * 2];
+        merge(arr1,arr2,arr3,n - 1,arr3.length - 1);
+        System.out.println("Merged Both Arrays : " + Arrays.toString(arr3));
     }
-    static void fillArr1(int[] arr,int[] merge,int n)
-    {
-        if(n < 0) return ;
-        merge[n] = arr[n];
-    }
-    static void fillArr2(int[]arr, int[] merge, int n)
+    static void merge(int[] arr1, int[] arr2,int[] arr3, int n, int length)
     {
         if(n < 0) return;
-        merge[n + arr.length] = arr[n];
+        arr3[n] = arr1[n];
+        arr3[length] = arr2[n];
+        merge(arr1,arr2,arr3,n - 1, length - 1);
+
+
     }
 }
