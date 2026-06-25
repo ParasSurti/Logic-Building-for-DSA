97403b7fb5136a834459f2480057768c9f3504a5
Paras Surti <paras16surti@gmail.com>
2026-06-25 19:21:40 +0530

    Q2.Compare two arrays — check if they contain the same elements (ignore order).

--- src/Phase_4/Level_4/Q02.java
+++ src/Phase_4/Level_4/Q02.java
@@ -4,52 +4,33 @@ package Phase_4.Level_4;
 import java.lang.Integer;
 import java.util.Scanner;
 
-public class Q02
+public class Q02 extends ArrayTemplate
 {
     public static void main(String[] args)
     {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the size of the Arrays : ");
-        int n = input.nextInt();
+        Q02 q = new Q02();
+        q.setup();
+        q.solve();
 
-        int[] arr1 = new int[n];
-        for (int i = 0; i < n; i++)
-        {
-            arr1[i] = input.nextInt();
-        }
-
-        int[] arr2 = new int[n];
-        for (int i = 0; i < n; i++)
-        {
-            arr2[i] = input.nextInt();
-        }
-
-        if(cmp(arr1,arr2,n - 1,0))
-        {
-            System.out.println("Both Array contain the same element!!");
-        }
-        else
-            System.out.println("Both Array doesn't contain same element!!");
     }
-    static boolean cmp(int[] arr1, int[] arr2, int n, int index)
+    @Override
+    void solve()
+    {
+        boolean check = cmp(arr1,arr2,n - 1, 0);
+        if(check) System.out.println("Both Arrays have same Element!!");
+        else System.out.println("Both Arrays doesn't have same Element!!");
+
+    }
+    static boolean cmp(int[] arr1, int[] arr2 , int n, int index)
     {
         if(n < 0) return true;
-
-        // Try to find and consume a match for arr1[index], arr2
-        if (!findAndMark(arr2,arr1[index],arr1.length - 1))
-        {
-            return false;
-        }
-        return cmp(arr1,arr2, n - 1, index + 1);
+        if(!cmpElement(arr1,arr2[index],0,arr1.length - 1)) return false;
+        return cmp(arr1,arr2,n - 1, index + 1);
     }
-    static boolean findAndMark(int[] arr2,int target, int n)
+    static boolean cmpElement(int[] arr1, int target,int start, int n)
     {
-        if (n < 0) return false;
-        if (arr2[n] == target)
-        {
-            arr2[n] = Integer.MIN_VALUE;
-            return true;
-        }
-        return findAndMark(arr2,target,n - 1);
+        if(start > n) return false;
+        if(arr1[start] == target) return true;
+        return cmpElement(arr1,target,start + 1, n);
     }
 }
