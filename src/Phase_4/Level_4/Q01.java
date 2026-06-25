a7a87df36dfd4f2f08a8b2183beb5e5bc8fb532a
Paras Surti <paras16surti@gmail.com>
2026-06-25 12:15:12 +0530

    Q01.Compare two arrays — check if they are equal (same elements & order).

--- src/Phase_4/Level_4/Q01.java
+++ src/Phase_4/Level_4/Q01.java
@@ -4,46 +4,25 @@ package Phase_4.Level_4;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Q01
+public class Q01 extends ArrayTemplate
 {
     public static void main(String[] args)
     {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the size of both Arrays : ");
-        int n = input.nextInt();
-
-        int[] arr1 = new int[n];
-        for (int i = 0; i < n; i++)
-        {
-            arr1[i] = input.nextInt();
-        }
-        int[] arr2 = new int[n];
-        for (int i = 0; i < n; i++)
-        {
-            arr2[i] = input.nextInt();
-        }
-
-        System.out.println("First Array  : " + Arrays.toString(arr1));
-        System.out.println("Second Array : " + Arrays.toString(arr2));
-
-
-        if(cmp(arr1,arr2,n - 1))
-        {
-            System.out.println("Both Arrays are Equal!!");
-        }
-        else System.out.println("Both Array are not Equal!!");
-
+        Q01 q = new Q01();
+        q.setup();
+        q.solve();
     }
-    static boolean cmp(int[] arr1, int[] arr2,int n)
+
+    @Override
+    void solve()
+    {
+        boolean check = cmp(arr1,arr2,n - 1);
+        if(!check)
+    }
+    static boolean cmp(int[] arr1, int[] arr2, int n)
     {
         if(n < 0) return true;
-
-        if(arr1[n] != arr2[n])
-        {
-            return false;
-        }
-     return cmp(arr1, arr2,n - 1);
-
+        if(arr1[n] != arr2[n]) return false;
+        return cmp(arr1,arr2,n - 1);
     }
-
 }
