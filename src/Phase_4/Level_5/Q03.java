b7dd1c6706f2dff1876b20ba10d291b8959769c0
Paras Surti <paras16surti@gmail.com>
2026-07-01 11:34:31 +0530

    Q3.Find the Second-largest element in an array.

---
+++ src/Phase_4/Level_5/Q03.java
@@ -0,0 +1,33 @@
+    //Find the Second-largest element in an array.
+    package Phase_4.Level_5;
+
+    public class Q03 extends ArrayTemplate
+    {
+        public static void main(String[] args)
+        {
+            Q03 q = new Q03();
+            q.setup();
+            q.solve();
+        }
+        @Override
+        void solve()
+        {
+            int largest = largest(arr,n - 1,arr[0],arr[1]);
+            System.out.println("Second Largest Element : " + largest);
+        }
+        static int largest(int[] arr, int n , int large, int seclarge)
+        {
+            if(n < 0) return seclarge;
+            if(arr[n] > large)
+            {
+                seclarge = large;
+                large = arr[n];
+            }
+            else if(arr[n] > seclarge)
+            {
+                seclarge = arr[n];
+            }
+            return largest(arr,n- 1, large,seclarge);
+        }
+
+    }
