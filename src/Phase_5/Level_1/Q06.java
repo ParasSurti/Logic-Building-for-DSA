7ac8752a583295d6a7f83cdf2e67f16663c000bc
Paras Surti <paras16surti@gmail.com>
2026-07-03 19:00:22 +0530

    Q6.Count how many words are in a sentence.

---
+++ src/Phase_5/Level_1/Q06.java
@@ -0,0 +1,28 @@
+//Count how many words are in a sentence.
+package Phase_5.Level_1;
+
+public class Q06 extends StringTemplate
+{
+    public static void main(String[] args)
+    {
+        Q06 q = new Q06();
+        q.setup();
+        q.solve();
+    }
+    @Override
+    void solve()
+    {
+        int words = count(str,l - 1, 0);
+        System.out.println(words);
+    }
+    static int count(String str, int index, int count)
+    {
+        if(index < 0) return count;
+        if(str.charAt(index) != ' ' &&
+                index == 0 || str.charAt(index - 1) == ' ')
+        {
+            count += 1;
+        }
+        return count(str, index - 1, count);
+    }
+}
