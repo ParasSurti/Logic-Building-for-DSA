b34c787bd68c957fbec661ebd05fd0e9a2add714
Paras Surti <paras16surti@gmail.com>
2026-07-04 21:38:43 +0530

    Q9.Print the ASCII value of each character in a string.

---
+++ src/Phase_5/Level_1/Q09.java
@@ -0,0 +1,23 @@
+//Print the ASCII value of each character in a string.
+package Phase_5.Level_1;
+
+public class Q09 extends StringTemplate
+{
+    public static void main(String[] args) {
+        Q09 q = new Q09();
+        q.setup();
+        q.solve();
+    }
+    @Override
+    void solve()
+    {
+        print(str,l - 1);
+    }
+    static void print(String str, int l)
+    {
+        if(l < 0) return;
+        print(str,l - 1);
+        System.out.println((int) str.charAt(l));
+//        System.out.println(str.charAt(l) + 0);
+    }
+}
