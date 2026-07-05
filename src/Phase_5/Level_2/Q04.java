664fec190acbdc9ac16be3ed103dd520675d2b14
Paras Surti <paras16surti@gmail.com>
2026-07-05 19:06:55 +0530

    Q4.Find the frequency of each character in a string (without using a map).

---
+++ src/Phase_5/Level_2/Q04.java
@@ -0,0 +1,39 @@
+//Find the frequency of each character in a string (without using a map).
+package Phase_5.Level_2;
+
+public class Q04 extends StringTemplate
+{
+    public static void main(String[] args)
+    {
+        Q04 q = new Q04();
+        q.setup();
+        q.solve();
+    }
+    @Override
+    void solve()
+    {
+         feq(str,l - 1);
+    }
+    static void feq(String str, int index)
+    {
+        if(index < 0) return;
+        char ch = Character.toLowerCase(str.charAt(index));
+        if(!alreadySeen(str,index - 1,ch))
+        {
+            System.out.println("character : " + ch + " index : " + countnum(str,str.length() - 1,ch));
+        }
+        feq(str,index - 1);
+    }
+    static int countnum(String str, int index, char ch)
+    {
+        if(index < 0) return 0;
+        if(ch == str.charAt(index)) return countnum(str,index - 1, ch) + 1;
+        return countnum(str,index - 1,ch);
+    }
+    static boolean alreadySeen(String str, int index, char ch)
+    {
+        if(index < 0) return false;
+        if(ch == str.charAt(index)) return true;
+        return alreadySeen(str,index -1 ,ch);
+    }
+}
