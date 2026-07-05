938f2d3040435308692338a329b3e0562294116f
Paras Surti <paras16surti@gmail.com>
2026-07-05 13:04:14 +0530

    Q2.Count the number of digits, letters, and special characters in a string.

---
+++ src/Phase_5/Level_2/Q02.java
@@ -0,0 +1,59 @@
+//Count the number of digits, letters, and special characters in a string.
+package Phase_5.Level_2;
+
+public class Q02 extends StringTemplate
+{
+    public static void main(String[] args)
+    {
+        Q02 q = new Q02();
+        q.setup();
+        q.solve();
+    }
+    @Override
+    void solve()
+    {
+        int letter = count(str,l - 1);
+        int digit = countdig(str,l - 1);
+        int spCha = countSp(str,l - 1);
+        System.out.println("There are " + letter + " letter's in this String!!");
+        System.out.println("There are " + digit + " number's in this String!!");
+        System.out.println("There are " + spCha + " special character in this String!!");
+    }
+    static int count(String str, int index)
+    {
+        if(index < 0) return 0;
+        char ch = Character.toLowerCase(str.charAt(index));
+        if(!isDigit(ch) && isLetter(ch))
+        {
+            return count(str,index - 1) + 1;
+        }
+        return count(str,index -1);
+    }
+    //Digit
+    static int countdig(String str, int index)
+    {
+        if(index < 0) return 0;
+        char ch = Character.toLowerCase(str.charAt(index));
+        if(isDigit(ch) && !isLetter(ch))
+        {
+            return countdig(str,index - 1) + 1;
+        }
+        return countdig(str,index -1);
+    }
+    //Special Character
+    static int countSp(String str, int index)
+    {
+        if(index < 0) return 0;
+        char ch = Character.toLowerCase(str.charAt(index));
+        if(!isDigit(ch) && !isLetter(ch) && ch != ' ')
+        {
+            return countSp(str,index - 1) + 1;
+        }
+        return countSp(str,index -1);
+    }
+    static boolean isLetter(char ch) { return ch >= 'a' && ch <= 'z';}
+    static boolean isDigit(char ch)
+    {
+        return ch >= '0' && ch <= '9';
+    }
+}
