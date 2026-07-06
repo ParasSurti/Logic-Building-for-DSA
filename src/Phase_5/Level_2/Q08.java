c9e0620dde3dc800096191886c173a595ec56811
Paras Surti <paras16surti@gmail.com>
2026-07-06 23:21:04 +0530

    Q8.Count how many substrings start and end with the same character (simple logic).

---
+++ src/Phase_5/Level_2/Q08.java
@@ -0,0 +1,32 @@
+//Count how many substrings start and end with the same character (simple logic).
+package Phase_5.Level_2;
+
+public class Q08 extends StringTemplate
+{
+    public static void main(String[] args)
+    {
+        Q08 q = new Q08();
+        q.setup();
+        q.solve();
+    }
+    @Override
+    void solve()
+    {
+        int count = words(str,0,l - 1);
+        System.out.println(count);
+    }
+    static int words(String str,int i, int index)
+    {
+        if(i > index) return 0;
+        return checkj(str,i,i,index) + words(str,i+1,index);
+    }
+    static int checkj(String str, int i , int j , int index)
+    {
+        if(j > index) return 0;
+        if(str.charAt(i) == str.charAt(j))
+        {
+            return checkj(str,i,j + 1,index) + 1;
+        }
+        return checkj(str,i,j + 1,index);
+    }
+}
