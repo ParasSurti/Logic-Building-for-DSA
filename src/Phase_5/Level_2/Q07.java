bdfa586b3c48a45a49de247dfb78da9044f82248
Paras Surti <paras16surti@gmail.com>
2026-07-06 14:13:36 +0530

    Q7.Count how many alphabets are before ‘m’ and after ‘m’ in a given string.

---
+++ src/Phase_5/Level_2/Q07.java
@@ -0,0 +1,40 @@
+//Count how many alphabets are before ‘m’ and after ‘m’ in a given string.
+package Phase_5.Level_2;
+
+public class Q07 extends StringTemplate
+{
+    public static void main(String[] args)
+    {
+        Q07 q = new Q07();
+        q.setup();
+        q.solve();
+    }
+    @Override
+    void solve()
+    {
+        int before = beforeCount(str,l - 1);
+        int after = afterCount(str,l - 1);
+        System.out.println("Total Before Character : " + before );
+        System.out.println("Total After Character : " + after);
+    }
+    static int beforeCount(String str,int index)
+    {
+        if(index < 0) return 0;
+        char ch = Character.toLowerCase(str.charAt(index));
+        if(ch >= 'a' && ch <= 'z')
+        {
+            if(ch < 'm') return beforeCount(str,index - 1) + 1;
+        }
+        return beforeCount(str,index - 1);
+    }
+    static int afterCount(String str,int index)
+    {
+        if(index < 0) return 0;
+        char ch = Character.toLowerCase(str.charAt(index));
+        if(ch >= 'a' && ch <= 'z')
+        {
+            if (ch > 'm') return afterCount(str, index - 1) + 1;
+        }
+        return afterCount(str,index - 1);
+    }
+}
