738c90165a9180324eb88e6ae2c894728b47bc57
Paras Surti <paras16surti@gmail.com>
2026-07-06 11:29:31 +0530

    Q5.Count how many spaces are there in a sentence.

---
+++ src/Phase_5/Level_2/Q05.java
@@ -0,0 +1,24 @@
+//Count how many spaces are there in a sentence.
+package Phase_5.Level_2;
+
+public class Q05 extends StringTemplate
+{
+    public static void main(String[] args)
+    {
+        Q05 q = new Q05();
+        q.setup();
+        q.solve();
+    }
+    @Override
+    void solve()
+    {
+        int space = count(str,l - 1);
+        System.out.println("There are total " + space + " spaces in the String!! ");
+    }
+    static int count(String str, int index)
+    {
+        if(index < 0) return 0;
+        if(str.charAt(index) == ' ') return count(str,index - 1) + 1;
+        return count(str,index - 1);
+    }
+}
