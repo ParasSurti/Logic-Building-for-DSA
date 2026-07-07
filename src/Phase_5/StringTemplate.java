86a94a00639dcc982f03ea83512efda48be3a3ae
Paras Surti <paras16surti@gmail.com>
2026-07-07 14:53:17 +0530

    StringTemplate : added abstract base template for solving string problems.

--- src/Phase_5/StringTemplate.java
+++ src/Phase_5/StringTemplate.java
@@ -1,20 +1,21 @@
-package Phase_5.Level_1;
+package Phase_5;
 
 import java.util.Scanner;
 
 public abstract class StringTemplate
 {
-    Scanner input = new Scanner(System.in);
-    String str ;
-    int l ;
+    protected Scanner input = new Scanner(System.in);
+    protected String str ;
+    protected int l ;
 
-    void setup()
+    protected void setup()
     {
         System.out.print("Enter a String : ");
         str = input.nextLine();
         l = str.length();
     }
-    abstract void solve();
+
+    protected abstract void solve();
 }
 
 
