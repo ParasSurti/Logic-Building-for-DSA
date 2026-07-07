ccde19c04d8f036c12e99433ae88c03d20c0629a
Paras Surti <paras16surti@gmail.com>
2026-07-07 15:05:04 +0530

    Q9.Print how many words start with a vowel in a sentence.

--- src/Phase_5/Level_2/Q09.java
+++ src/Phase_5/Level_2/Q09.java
@@ -1,6 +1,8 @@
 //Print how many words start with a vowel in a sentence.
 package Phase_5.Level_2;
 
+import Phase_5.StringTemplate;
+
 public class Q09 extends StringTemplate
 {
     public static void main(String[] args)
@@ -10,7 +12,7 @@ public class Q09 extends StringTemplate
         q.solve();
     }
     @Override
-    void solve()
+    protected void solve()
     {
         //A, E, I, O, U
         int count = count(str,0);
