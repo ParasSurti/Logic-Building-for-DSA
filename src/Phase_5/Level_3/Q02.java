a2fb4e82341d8cfb012213c8b18e9aa4f75fad65
Paras Surti <paras16surti@gmail.com>
2026-07-08 15:19:05 +0530

    Q2.Reverse each word in a sentence.

---
+++ src/Phase_5/Level_3/Q02.java
@@ -0,0 +1,33 @@
+//Reverse each word in a sentence.
+package Phase_5.Level_3;
+
+import Phase_5.StringTemplate;
+
+public class Q02 extends StringTemplate
+{
+    public static void main(String[] args)
+    {
+        Q02 q = new Q02();
+        q.setup();
+        q.solve();
+    }
+
+    @Override
+    protected void solve()
+    {
+        String rev = sentence(str,l,0);
+        System.out.println(rev);
+    }
+    static String sentence(String str, int index, int start)
+    {
+        if(start >= index) return "";
+        if(start + 1 == index || str.charAt(start + 1) == ' ') return rev(str,start) + " " + sentence(str,index,start + 1);
+        return sentence(str,index,start + 1);
+    }
+    static String rev(String str, int start)
+    {
+        if(start < 0 || str.charAt(start) == ' ') return "";
+        return str.charAt(start) + rev(str,start - 1);
+    }
+
+}
