c93e10de5975bf5aff5d6c0f1c3b48c8eb5746e4
Paras Surti <paras16surti@gmail.com>
2026-07-09 14:27:55 +0530

    Q1.Count how many vowels and consonants are in a string.

--- src/Phase_5/Level_2/Q01.java
+++ src/Phase_5/Level_2/Q01.java
@@ -1,9 +1,9 @@
 //Count how many vowels and consonants are in a string.
 package Phase_5.Level_2;
 
-import Phase_5.StringTemplate;
+import Phase_5.OneStringTemplate;
 
-public class Q01 extends StringTemplate
+public class Q01 extends OneStringTemplate
 {
     public static void main(String[] args)
     {
