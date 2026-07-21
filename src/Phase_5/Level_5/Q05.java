a33b89d09ee7b1330dde84b906933d9cd517a5ed
Paras Surti <paras16surti@gmail.com>
2026-07-21 17:49:25 +0530

    Q5.Swap first and last words in a sentence.

--- src/Phase_5/Level_5/Q05.java
+++ src/Phase_5/Level_5/Q05.java
@@ -14,22 +14,51 @@ public class Q05 extends OneStringTemplate
     @Override
     protected void solve()
     {
-        String[] words = str.split("\\s+");
-        swap(words,0,words.length - 1);
-        for (int i = 0; i < words.length; i++)
-        {
-            System.out.print(words[i]);
-            if (i != words.length - 1) {
-                System.out.print(" ");
-            }
-        }
-    }
+//        String[] words = str.split("\\s+");
+//        swap(words,0,words.length - 1);
+//        for (int i = 0; i < words.length; i++)
+//        {
+//            System.out.print(words[i]);
+//            if (i != words.length - 1)
+//            {
+//                System.out.print(" ");
+//            }
+//        }
 
-    static void swap(String[] words ,int first, int last)
+        int firstEnd = firstSpace(str,0);
+        int lastStart = lastSpace(str,l - 1);
+        String middle = str.substring(firstEnd,lastStart + 1);
+//        String fullWord = print(str,first,last,word);
+        System.out.println(last(str,l - 1) + middle + first(str,0));
+    }
+    static String first(String str, int start)
     {
-        String temp = words[first];
-        words[first] = words[last];
-        words[last] = temp;
+        if(str.charAt(start) == ' ') return "";
+        return str.charAt(start) + first(str,start + 1);
     }
 
+    static String last(String str, int last)
+    {
+        if(str.charAt(last) == ' ') return "";
+        return last(str,last - 1) + str.charAt(last) ;
+    }
+
+    static int firstSpace(String str, int start)
+    {
+        if(str.charAt(start) == ' ') return start;
+        return firstSpace(str,start + 1);
+    }
+
+    static int lastSpace(String str, int end)
+    {
+        if(str.charAt(end) == ' ') return end;
+        return lastSpace(str,end - 1);
+    }
+
+//    static void swap(String[] words ,int first, int last)
+//    {
+//        String temp = words[first];
+//        words[first] = words[last];
+//        words[last] = temp;
+//    }
 }
