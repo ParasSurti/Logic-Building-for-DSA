dfd304b55c1fc30158edeb360c25e32c25eeb57f
Paras Surti <paras16surti@gmail.com>
2026-07-22 20:29:33 +0530

    Q8.Capitalize the first letter of each word.

---
+++ src/Phase_5/Level_5/Q08.java
@@ -0,0 +1,33 @@
+//Capitalize the first letter of each word.
+package Phase_5.Level_5;
+
+import Phase_6.OneStringTemplate;
+
+public class Q08 extends OneStringTemplate
+{
+    public static void main(String[] args)
+    {
+        Q08 q = new Q08();
+        q.setup();
+        q.solve();
+    }
+    @Override
+    protected void solve()
+    {
+        String print = sentence(str,0);
+        System.out.println(print);
+    }
+    static String sentence(String str, int i)
+    {
+        if(i == str.length()) return "";
+        return letter(str,i) + sentence(str, i + 1) ;
+    }
+    static char letter(String str, int i )
+    {
+        if(i == 0 || str.charAt(i - 1) == ' ')
+        {
+            return Character.toUpperCase(str.charAt(i));
+        }
+        return Character.toLowerCase(str.charAt(i));
+    }
+}
