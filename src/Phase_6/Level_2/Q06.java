e3e8df66048b5adf7e5a0eae7147f3837912ac06
Paras Surti <paras16surti@gmail.com>
2026-07-31 17:22:18 +0530

    Q6.Count words that start and end with the same letter.

---
+++ src/Phase_6/Level_2/Q06.java
@@ -0,0 +1,37 @@
+//Count words that start and end with the same letter.
+package Phase_6.Level_2;
+
+import Phase_6.OneStringTemplate;
+
+import java.util.Arrays;
+
+public class Q06 extends OneStringTemplate
+{
+    public static void main(String[] args)
+    {
+        Q06 q = new Q06();
+        q.setup();
+        q.solve();
+    }
+    @Override
+    protected void solve()
+    {
+        String[] words = str.split("\\s++");
+        int count = sameLetter(words,0,0);
+        System.out.println(count);
+    }
+    static int sameLetter(String[] words,int start, int count)
+    {
+        if(start >= words.length) return count;
+        String str = words[start];
+        if(check(str))
+        {
+            count++;
+        }
+        return sameLetter(words,start+1,count);
+    }
+    static boolean check(String str)
+    {
+        return str.charAt(0) == str.charAt(str.length() - 1);
+    }
+}
